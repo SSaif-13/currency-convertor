b8c3078d388ff9b4e6a1e63aaa370b7a3d2b4859
Sadat Saif <sadat.saif@ucalgary.ca>
2023-09-16 23:07:34 -0600

    Added documentations

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@ import React, { useEffect, useState } from "react";
 import "./App.css";
 import CurrencyRow from "./CurrencyRow";
 
+//the base URL for the exchange rate API
 const BASE_URL = "https://api.exchangerate.host/latest";
 
+//Define and initialize state variables using the useState hook
 function App() {
-  const [currencyOptions, setCurrencyOptions] = useState([]);
-  const [fromCurrency, setFromCurrency] = useState();
-  const [toCurrency, settoCurrency] = useState();
-  const [exchangeRate, setExchangeRate] = useState();
-  const [amount, setAmount] = useState(1);
-  const [amountInFromCurrency, setAmountInFromCurrency] = useState(true);
+  const [currencyOptions, setCurrencyOptions] = useState([]); // Available currency options
+  const [fromCurrency, setFromCurrency] = useState(); // Selected 'from' currency
+  const [toCurrency, settoCurrency] = useState(); // Selected 'to' currency
+  const [exchangeRate, setExchangeRate] = useState(); // Exchange rate
+  const [amount, setAmount] = useState(1); // initial amount to convert
+  const [amountInFromCurrency, setAmountInFromCurrency] = useState(true); // Track if input is in 'from' currency
 
+  // Calculate 'to' and 'from' amounts based on exchange rate and input
   let toAmount, fromAmount;
   if (amountInFromCurrency) {
     fromAmount = amount;
@@ -21,36 +24,43 @@ function App() {
     fromAmount = amount / exchangeRate;
   }
 
+  // Fetch initial exchange rate data when the component mounts
   useEffect(() => {
     fetch(BASE_URL)
       .then((res) => res.json())
       .then((data) => {
+        // Extract the first currency from the API data
         const firstCurrency = Object.keys(data.rates)[0];
+        // Set state variables with initial data
         setCurrencyOptions([data.base, ...Object.keys(data.rates)]);
         setFromCurrency(data.base);
         settoCurrency(firstCurrency);
         setExchangeRate(data.rates[firstCurrency]);
       });
-  }, []);
+  }, []); // Empty dependency array means this effect runs once when the component mounts
 
+  // Fetch exchange rate data when 'fromCurrency' or 'toCurrency' changes
   useEffect(() => {
     if (fromCurrency != null && toCurrency != null) {
       fetch(`${BASE_URL}?base=${fromCurrency}&symbols=${toCurrency}`)
         .then((res) => res.json())
         .then((data) => setExchangeRate(data.rates[toCurrency]));
     }
-  }, [fromCurrency, toCurrency]);
+  }, [fromCurrency, toCurrency]); // Dependency array triggers the effect when these variables change
 
+  // Handler for changing 'from' amount input
   function handleFromAmountChange(e) {
     setAmount(e.target.value);
     setAmountInFromCurrency(true);
   }
 
+  // Handler for changing 'to' amount input
   function handleToAmountChange(e) {
     setAmount(e.target.value);
     setAmountInFromCurrency(false);
   }
 
+  // Render the component JSX
   return (
     <>
       <h1>Convert</h1>
--- src/CurrencyRow.js
+++ src/CurrencyRow.js
@@ -1,5 +1,18 @@
 import React from "react";
 
+/**
+ * CurrencyRow component for displaying a currency input row with a select dropdown.
+ *
+ * @param {Object} props - Component props.
+ * @param {string[]} props.currencyOptions - Array of available currency options.
+ * @param {string} props.selectCurrency - Currently selected currency.
+ * @param {function} props.onChangeCurrency - Function to handle currency selection change.
+ * @param {function} props.onChangeAmount - Function to handle amount input change.
+ * @param {number} props.amount - Amount value for the input field.
+ * @returns {JSX.Element} CurrencyRow component.
+ */
+
+// Destructure props to access individual values
 export default function CurrencyRow(props) {
   const {
     currencyOptions,
@@ -9,6 +22,7 @@ export default function CurrencyRow(props) {
     amount,
   } = props;
 
+  // Render the CurrencyRow component JSX
   return (
     <div>
       <input
@@ -18,6 +32,7 @@ export default function CurrencyRow(props) {
         onChange={onChangeAmount}
       ></input>
       <select value={selectCurrency} onChange={onChangeCurrency}>
+        {/* Map through currency options and generate <option> elements */}
         {currencyOptions.map((option, index) => (
           <option key={index} value={option}>
             {option}
