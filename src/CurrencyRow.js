22eae7cf381945f3ad4e9b29212a749c3797e2a0
Sadat Saif <sadat.saif@ucalgary.ca>
2023-09-16 22:46:36 -0600

    Completed the functionality of converting currency

--- src/CurrencyRow.js
+++ src/CurrencyRow.js
@@ -1,11 +1,22 @@
 import React from "react";
 
 export default function CurrencyRow(props) {
-  const { currencyOptions, selectCurrency, onChangeCurrency } = props;
+  const {
+    currencyOptions,
+    selectCurrency,
+    onChangeCurrency,
+    onChangeAmount,
+    amount,
+  } = props;
 
   return (
     <div>
-      <input type="number" className="input"></input>
+      <input
+        type="number"
+        className="input"
+        value={amount}
+        onChange={onChangeAmount}
+      ></input>
       <select value={selectCurrency} onChange={onChangeCurrency}>
         {currencyOptions.map((option, index) => (
           <option key={index} value={option}>
